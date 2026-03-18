3fa626de1d21137a2d33a3fa8e94007f88dfc6a7
Khushi5155 <kkaayana5155@gmail.com>
2026-03-18 18:25:39 +0530

    Added node voltage computation from solved matrix

--- integration/solver.py
+++ integration/solver.py
@@ -10,4 +10,15 @@ def solve_matrix(G, I):
     except Exception as e:
         print("Error solving matrix:", e)
         return None
-        
+    
+def compute_node_voltages(V, node_mapping):
+    node_voltages = {}
+
+    for node, idx in node_mapping.items():
+        node_voltages[node] = float(V[idx])
+
+    # Ground node
+    node_voltages[0] = 0.0
+
+    return node_voltages
+    
